69f7e4b2c64ab3334e8ab6f65c9618119b39344b
Glenn Strauss <gstrauss@gluelogic.com>
2025-01-21 22:14:33 -0500

    lighttpd 1.4.77

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export NDK=/home/nredko/bin/android-ndk-r21d
 export ZLIB=zlib-1.2.11
 export PCRE=pcre-8.44
 export OPENSSL=openssl-1.1.1i
-export LIGHTTPD=lighttpd-1.4.58
+export LIGHTTPD=lighttpd-1.4.77
 
 #############################################################
 # download
@@ -88,7 +88,6 @@ fi
 rm -f src/plugin-static.h 
 cp $BLD/plugin-static.h ./src/
 CPPFLAGS=-DLIGHTTPD_STATIC LIGHTTPD_STATIC=yes ./configure -C --host=$TARGET --enable-static=yes --enable-shared=no --disable-shared --prefix=$BLD --disable-ipv6 --with-pcre=$BLD --with-zlib=$BLD --with-openssl=$BLD
-sed -i.bak '/lighttpd-mod_webdav/d' ./src/Makefile
 make install-strip
 
 
